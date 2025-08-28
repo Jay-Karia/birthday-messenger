c224a1cd4b1b5993823aee9e8da9bbd7119cba9f
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-08-29 01:53:50 +0530

    chore: changed min width and height of the window

--- src/index.js
+++ src/index.js
@@ -41,6 +41,8 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minHeight: 600,
+    minWidth: 800,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: true,
